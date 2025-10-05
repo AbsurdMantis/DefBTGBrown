b4d68eaca9f504e2a6434326deeaa95ffc638a26
M.AB <raphaelbholmes@gmail.com>
2025-10-05 04:50:36 -0300

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DefBTGBrown
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
